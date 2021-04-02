02c3e99b13f51e48c7b704fe0c1365996d7685d9
essneider-quintero07 <80271192+essneider-quintero07@users.noreply.github.com>
2021-04-01 20:00:57 -0500

    Delete ejercicio_12.py

--- 70 ejercicios/ejercicio_12.py
+++
@@ -1,5 +0,0 @@
-import math 
-gravedad = 9.8
-altura = float(input("ingresa la altura desde la cual el objeto es lanzado ( en metros )\n"))
-tiempo = math.sqrt ((2*altura)/gravedad)
-print (" el tiempo que se demora caer el objeto es =",tiempo,"segundos")
