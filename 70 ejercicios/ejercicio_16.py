31c7a83d4ec121888eb09fe0c38dcf40bdf57315
essneider-quintero07 <80271192+essneider-quintero07@users.noreply.github.com>
2021-04-01 20:01:34 -0500

    Delete ejercicio_16.py

--- 70 ejercicios/ejercicio_16.py
+++
@@ -1,8 +0,0 @@
-#√((x_2-x_1)²+(y_2-y_1)²)
-import math
-x1= float(input("por favor ingresar el valor de x1 "))
-y1=float(input("por favor ingresar el valor de y1 "))
-x2=float(input("por favor ingresar el valor de x2 "))
-y2=float(input("por favor ingresar el valor de y2 " ))
-distancia =  math.sqrt(((x2-x1)**2)+((y2-y1)**2))
-print(distancia)
