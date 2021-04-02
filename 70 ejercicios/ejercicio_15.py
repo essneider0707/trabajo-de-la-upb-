04bf7b301d42407413e908a7d04fccbd100bde5d
essneider-quintero07 <80271192+essneider-quintero07@users.noreply.github.com>
2021-04-01 20:01:26 -0500

    Delete ejercicio_15.py

--- 70 ejercicios/ejercicio_15.py
+++
@@ -1,4 +0,0 @@
-masa = float(input("ingrea el valor de la masa del objeto ( en kg)"))
-velocidad = float(input("ingresa el valor de la velocidad del objeto ( en m/s)"))
-Ec = (masa * pow(velocidad,2))/2
-print("el valor de la energia es = ",Ec ,"julios")
