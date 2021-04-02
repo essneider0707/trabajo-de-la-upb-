962ba2882cc3b912fa403bbd609b68bbf24e5c94
essneider-quintero07 <80271192+essneider-quintero07@users.noreply.github.com>
2021-04-01 20:01:21 -0500

    Delete ejercicio_14.py

--- 70 ejercicios/ejercicio_14.py
+++
@@ -1,4 +0,0 @@
-aceleracion = float(input("ingresa el valor de la aceleracion (en m/s^2"))
-tiempo = float(input ("ingresa el valor del tiempo (en s )"))
-velocidad_final = 0 + aceleracion * tiempo
-print("la velocidad final es =" , velocidad_final,"m/s")
